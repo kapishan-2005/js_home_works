9476a6c347ab7ba251f2461dc692ee137aae93ec
kapishan-2005 <kapishan.official@gmail.com>
2025-11-17 19:35:55 +0530

    home work

---
+++ script.js
@@ -0,0 +1,19 @@
+let factor = "thanush";
+let fplayer="dhoni";
+let fmovie="anbe sivam";
+function favourite() {
+console.log("Favourite actor is:"+factor);
+console.log("Favourite plyer is:"+fplayer);
+console.log("Favourite movie is:"+fmovie);
+};
+favourite();
+function area(length,breadth){
+console.log(length*breadth);
+};
+area(10,30);
+
+function sum(l,w){
+    return(l+w);
+}
+let x= sum(10,20);
+console.log(x);
